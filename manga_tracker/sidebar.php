fdb64194f4e12bcbfe0179b8d069bb76de6f3b79
jgithubj2022 <112044755+jgithubj2022@users.noreply.github.com>
2025-12-26 14:10:47 -0800

    Update sidebar.php

--- manga_tracker/sidebar.php
+++ manga_tracker/sidebar.php
@@ -1,7 +1,7 @@
 <!-- sidebar.php -->
 <aside class="sidebar">
 	<div class="sidebar-header">
-		<img src="nonmangaimages/manga_bite_logo.png" alt="Manga Logo" width="200" height ="140">
+		<img src="nonmangaimages/manga_bite_logo.png" alt="Manga Logo" width="230" height ="70">
 		<div class="sidebar-subtitle">Library and tools</div>
 	</div>
 
@@ -28,4 +28,5 @@
 		<div class="sidebar-footnote">Version 0.2 JS <3 </div>
 	</div>
 </aside>
+
 
